b565590328e364c3618a617bd88fd75ff30cc9b8
Nicolas Fränkel <nicolas@frankel.ch>
2022-10-13 17:17:20 +0200

    Fix compilation error by adding a try/catch block

--- src/main/java/ch/frankel/blog/lambdasexceptions/Main.java
+++ src/main/java/ch/frankel/blog/lambdasexceptions/Main.java
@@ -6,7 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Stream.of("java.lang.String", "ch.frankel.blog.Dummy", "java.util.ArrayList")
-                .map(it -> Class.forName(it))
+                .map(it -> {
+                    try {
+                        return Class.forName(it);
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
                 .forEach(System.out::println);
     }
 }
