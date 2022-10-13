f3c139a832e09481e35d0217a7da45a439061b35
Nicolas Fränkel <nicolas@frankel.ch>
2022-10-13 17:39:15 +0200

    Introduce record to get back readability

--- src/main/java/ch/frankel/blog/lambdasexceptions/Main.java
+++ src/main/java/ch/frankel/blog/lambdasexceptions/Main.java
@@ -1,18 +1,25 @@
 package ch.frankel.blog.lambdasexceptions;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         Stream.of("java.lang.String", "ch.frankel.blog.Dummy", "java.util.ArrayList")
-                .map(it -> {
-                    try {
-                        return Class.forName(it);
-                    } catch (ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
+                .map(it -> new ForNamer().apply(it))
                 .forEach(System.out::println);
+    }
+
+    record ForNamer() implements Function<String, Class<?>> {
+
+        @Override
+        public Class<?> apply(String string) {
+            try {
+                return Class.forName(string);
+            } catch (ClassNotFoundException e) {
+                return null;
+            }
+        }
     }
 }
