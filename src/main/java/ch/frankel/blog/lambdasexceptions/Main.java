b2b649c1ad2c84fb8963025607294fb1e8d5152c
Nicolas Fränkel <nicolas@frankel.ch>
2022-10-14 17:35:05 +0200

    Improve the readability by replacing Java's Stream with Vavr's

--- src/main/java/ch/frankel/blog/lambdasexceptions/Main.java
+++ src/main/java/ch/frankel/blog/lambdasexceptions/Main.java
@@ -2,20 +2,21 @@ package ch.frankel.blog.lambdasexceptions;
 
 import io.vavr.CheckedFunction1;
 import io.vavr.collection.Stream;
+import io.vavr.control.Either;
 import io.vavr.control.Try;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Stream.of("java.lang.String", "ch.frankel.blog.Dummy", "java.util.ArrayList")
+        var result = Stream.of("java.lang.String", "ch.frankel.blog.Dummy", "java.util.ArrayList")
                 .map(CheckedFunction1.liftTry(Class::forName))
                 .map(Try::toEither)
-                .forEach(e -> {
-                    if (e.isLeft()) {
-                        System.out.println("not found:" + e.getLeft().getMessage());
-                    } else {
-                        System.out.println("class:" + e.get().getName());
-                    }
-                });
+                .partition(Either::isLeft)
+                .map1(left -> left.map(Either::getLeft))
+                .map2(right -> right.map(Either::get));
+
+        result._1().forEach(it -> System.out.println("not found: " + it.getMessage()));
+        result._2().forEach(it -> System.out.println("class: " + it.getName()));
     }
 }
